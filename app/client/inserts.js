cc82cebaa4a6f743eb48bb8d1a0bd615dc587761
Zuzana <mizakova.zuzana@gmail.com>
2016-04-01 00:23:13 +0200

    merged with template

---
+++ app/client/inserts.js
@@ -0,0 +1,26 @@
+//inserts - used only once, for database filling
+
+
+//Meteor.call("insertArea"," Baskovce","Pekna dedina" , 1,2, 3, 360,"tazko povedat", Meteor.userId(), 2);
+//Meteor.call("insertArea"," Cestice","Pekna dedina" , 1,3, 3, 390,"tazko povedat", "X", 2);
+
+
+//Meteor.call("insertCardinals", 0, "SEVER", true);
+//Meteor.call("insertCardinals", 45, "SEVERO-VYCHOD", false);
+//Meteor.call("insertCardinals", 90, "VYCHOD", true);
+//Meteor.call("insertCardinals", 135, "JUHO-VYCHOD", false);
+//Meteor.call("insertCardinals", 180, "JUH", true);
+//Meteor.call("insertCardinals", 225, "JUHO-ZAPAD", false);
+//Meteor.call("insertCardinals", 270, "ZAPAD", true);
+//Meteor.call("insertCardinals", 315, "SEVERO-ZAPAD", false);
+
+//Meteor.call("insertHabitat", "vodny", 2, "default");
+//Meteor.call("insertHabitat", "les", 3, "default");
+//Meteor.call("insertHabitat", "luka", 1, "default");
+
+//Meteor.call("insertReport",23, "EDTy3kYvri4sHX7Wd", null, Meteor.userId(), new Date());
+//Meteor.call("insertUserSettings", "default-values", "Braun-Blanquet", 8);
+//Meteor.call("insertUserSettings", Meteor.userId(),"Braun-Blanquet", 8 );
+
+
+
